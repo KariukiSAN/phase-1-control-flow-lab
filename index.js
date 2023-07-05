01cf245dd4ba271c08c0e65234edd270f5e66b02
Annette Kariuki <kariukian1@gmail.com>
2023-07-05 12:10:34 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city=== 'NYC'? "Let's go to" + city+ "!": "Nope not going to"
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 5:
+      return "Thank you so much.";
+    case 10:
+      return "Thank you.";
+    case 15:
+      return "Thank you, you're too kind.";
+    default:
+      return "Thank you."
+  }
 }
